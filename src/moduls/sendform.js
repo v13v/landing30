3f0971bcf32d8a8feb25113fbc46fae981cfc229
Вадим <vadMoroz13@yandex.ru>
2020-07-21 20:55:09 +0300

    30les

--- src/moduls/sendform.js
+++ src/moduls/sendform.js
@@ -11,7 +11,7 @@ const sendform = () => {
         formEmail = document.getElementById('form1-email'),
         form1Phone = document.getElementById('form1-phone');
     //Переменные второй формы-->
-    const  form2 = document.getElementById('form2'),
+    const form2 = document.getElementById('form2'),
         formName2 = document.getElementById('form2-name'),
         formEmail2 = document.getElementById('form2-email'),
         form1Phone2 = document.getElementById('form2-phone'),
@@ -24,67 +24,66 @@ const sendform = () => {
 
     form.addEventListener('submit', event => {
         event.preventDefault();
-        formName.value = '';
-        formEmail.value = '';
-        form1Phone.value = '';
         form.appendChild(statusMassege);
         statusMassege.textContent = loadMesage;
-        const formData = new FormData(form);
+        const formData = new FormData(event.target);
         const body = {};
-        for (const val of formData.entries()) {
-            body[val[0]] = val[1];
-        }
-
+        formData.forEach((val, key) => {
+            body[key] = val;
+        })
         postData(body)
             .then(respons => {
                 if (respons.status !== 200) {
                     throw new Error('status network not 200');
                 }
                 statusMassege.textContent = successMessage;
+                let tickTack = setTimeout(() => {
+                    statusMassege.textContent = ``;
+                    clearTimeout(tickTack);
+                }, 5000);
             })
             .catch(error => {
                 statusMassege.textContent = errorMessage;
                 console.error(error);
             });
+        event.target.reset();
     });
     form2.addEventListener('submit', event => {
         event.preventDefault();
-        formName2.value = '';
-        formEmail2.value = '';
-        form1Phone2.value = '';
-        formMessage2.value = '';
         form2.appendChild(statusMassege);
         statusMassege.textContent = loadMesage;
-        const formData = new FormData(form2);
+        const formData = new FormData(event.target);
         const body = {};
-        for (const val of formData.entries()) {
-            body[val[0]] = val[1];
-        }
+        formData.forEach((val, key) => {
+            body[key] = val;
+        })
         postData(body)
             .then(respons => {
                 if (respons.status !== 200) {
                     throw new Error('status network not 200');
                 }
                 statusMassege.textContent = successMessage;
+                let time = setTimeout(() => {
+                    statusMassege.textContent = ``;
+                    clearTimeout(time);
+                }, 5000);
             })
             .catch(error => {
                 statusMassege.textContent = errorMessage;
                 console.error(error);
             });
+        event.target.reset();
     });
     form3.addEventListener('submit', event => {
         event.preventDefault();
-        formName3.value = '';
-        formEmail3.value = '';
-        formPhone3.value = '';
         form3.appendChild(statusMassege);
         statusMassege.style.color = 'white';
         statusMassege.textContent = loadMesage;
-        const formData = new FormData(form3);
+        const formData = new FormData(event.target);
         const body = {};
-        for (const val of formData.entries()) {
-            body[val[0]] = val[1];
-        }
+        formData.forEach((val, key) => {
+            body[key] = val;
+        })
         postData(body)
             .then(respons => {
                 if (respons.status !== 200) {
@@ -96,6 +95,11 @@ const sendform = () => {
                 statusMassege.textContent = errorMessage;
                 console.error(error);
             });
+        event.target.reset();
+        let time = setTimeout(() => {
+            statusMassege.textContent = ``;
+            clearTimeout(time);
+        }, 5000)
     });
 
 
@@ -113,7 +117,7 @@ const sendform = () => {
         const form2Name = document.getElementById('form2-name');
         const form3Name = document.getElementById('form3-name');
         const form1Name = document.getElementById('form1-name');
-        const showLog = function() {
+        const showLog = function () {
             this.value = this.value.replace(/[^а-яё ]/gi, '');
         };
         form2Message.addEventListener('input', showLog);
@@ -127,7 +131,7 @@ const sendform = () => {
         const form3Phone = document.getElementById('form3-phone');
         const form2Phone = document.getElementById('form2-phone');
         const form1Phone = document.getElementById('form1-phone');
-        const showPhone = function() {
+        const showPhone = function () {
             this.value = this.value.replace(/[^+0-9]/g, "");
         };
         form3Phone.addEventListener('input', showPhone);
