3b1284198f58453b14ff146b55b3d1d550926e68
Вадим <vadMoroz13@yandex.ru>
2020-07-23 01:07:03 +0300

    dip

--- src/moduls/sendform.js
+++ src/moduls/sendform.js
@@ -1,145 +1,58 @@
 const sendform = () => {
-    const errorMessage = 'Что то пошло не так....',
-        loadMesage = 'Идет загрузка... ',
-        successMessage = 'Спасибо мы скоро с Вами свяжемся',
+    const errorMessage = 'Что то пошло не так....'
+    const loadMesage = 'Идет загрузка... '
+    const successMessage = 'Спасибо мы скоро с Вами свяжемся'
 
-        statusMassege = document.createElement('div');
-    statusMassege.style.cssText = 'font-size: 2rem;';
-    //Переменные первой формы-->
-    const form = document.getElementById('form1'),
-        formName = document.getElementById('form1-name'),
-        formEmail = document.getElementById('form1-email'),
-        form1Phone = document.getElementById('form1-phone');
-    //Переменные второй формы-->
-    const form2 = document.getElementById('form2'),
-        formName2 = document.getElementById('form2-name'),
-        formEmail2 = document.getElementById('form2-email'),
-        form1Phone2 = document.getElementById('form2-phone'),
-        formMessage2 = document.getElementById('form2-message');
-    //Переменные третьей формы-->
-    const form3 = document.getElementById('form3'),
-        formName3 = document.getElementById('form3-name'),
-        formEmail3 = document.getElementById('form3-email'),
-        formPhone3 = document.getElementById('form3-phone');
+    const statusMessage = document.createElement('div')
+    statusMessage.style.cssText = 'font-size: 2rem;'
 
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        form.appendChild(statusMassege);
-        statusMassege.textContent = loadMesage;
-        const formData = new FormData(event.target);
-        const body = {};
-        formData.forEach((val, key) => {
-            body[key] = val;
-        })
-        postData(body)
-            .then(respons => {
-                if (respons.status !== 200) {
-                    throw new Error('status network not 200');
-                }
-                statusMassege.textContent = successMessage;
-                let tickTack = setTimeout(() => {
-                    statusMassege.textContent = ``;
-                    clearTimeout(tickTack);
-                }, 5000);
-            })
-            .catch(error => {
-                statusMassege.textContent = errorMessage;
-                console.error(error);
-            });
-        event.target.reset();
-    });
-    form2.addEventListener('submit', event => {
-        event.preventDefault();
-        form2.appendChild(statusMassege);
-        statusMassege.textContent = loadMesage;
-        const formData = new FormData(event.target);
-        const body = {};
-        formData.forEach((val, key) => {
-            body[key] = val;
-        })
-        postData(body)
-            .then(respons => {
-                if (respons.status !== 200) {
-                    throw new Error('status network not 200');
-                }
-                statusMassege.textContent = successMessage;
-                let time = setTimeout(() => {
-                    statusMassege.textContent = ``;
-                    clearTimeout(time);
-                }, 5000);
-            })
-            .catch(error => {
-                statusMassege.textContent = errorMessage;
-                console.error(error);
-            });
-        event.target.reset();
-    });
-    form3.addEventListener('submit', event => {
-        event.preventDefault();
-        form3.appendChild(statusMassege);
-        statusMassege.style.color = 'white';
-        statusMassege.textContent = loadMesage;
-        const formData = new FormData(event.target);
-        const body = {};
-        formData.forEach((val, key) => {
-            body[key] = val;
-        })
-        postData(body)
-            .then(respons => {
-                if (respons.status !== 200) {
-                    throw new Error('status network not 200');
-                }
-                statusMassege.textContent = successMessage;
-            })
-            .catch(error => {
-                statusMassege.textContent = errorMessage;
-                console.error(error);
-            });
-        event.target.reset();
-        let time = setTimeout(() => {
-            statusMassege.textContent = ``;
-            clearTimeout(time);
-        }, 5000)
-    });
-
+    const formValidate = event => {
+        const {target} = event
+        if(target.name === 'user_name' || target.name === 'user_message') {
+            target.value = target.value.replace(/[^А-Яа-яёЁ\s]/, "")
+        } else if(target.name === 'user_email') {
+            target.value = target.value.replace(/[А-Яа-яёЁ\s]/, "")
+        } else if(target.name === 'user_phone') {
+            target.value = target.value.replace(/[^0-9+]/, "")
+        }
+    }
 
     const postData = body => fetch('./server.php', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-type': 'application/json'
         },
         body: JSON.stringify(body),
-        credentials: 'include',
-    });
+        credentials: 'include'
+    })
 
-    function checkNameForm() {
-        const form2Message = document.getElementById('form2-message');
-        const form2Name = document.getElementById('form2-name');
-        const form3Name = document.getElementById('form3-name');
-        const form1Name = document.getElementById('form1-name');
-        const showLog = function () {
-            this.value = this.value.replace(/[^а-яё ]/gi, '');
-        };
-        form2Message.addEventListener('input', showLog);
-        form2Name.addEventListener('input', showLog);
-        form3Name.addEventListener('input', showLog);
-        form1Name.addEventListener('input', showLog);
-    }
-    checkNameForm();
+    document.querySelectorAll('form').forEach(form => {
+        form.addEventListener('input', formValidate)
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            const {target} = e
+            target.append(statusMessage)
+            statusMessage.textContent = loadMesage
 
-    function checkPhonForm() {
-        const form3Phone = document.getElementById('form3-phone');
-        const form2Phone = document.getElementById('form2-phone');
-        const form1Phone = document.getElementById('form1-phone');
-        const showPhone = function () {
-            this.value = this.value.replace(/[^+0-9]/g, "");
-        };
-        form3Phone.addEventListener('input', showPhone);
-        form1Phone.addEventListener('input', showPhone);
-        form2Phone.addEventListener('input', showPhone);
-    }
-    checkPhonForm();
+            const formData = new FormData(target)
+            const body = {}
+            formData.forEach((val, key) => {
+                body[key] = val
+            })
 
+            postData(body)
+                .then(response => {
+                    if(response.status !== 200) {
+                        throw new Error('status network not 200')
+                    }
+                    statusMessage.textContent = successMessage;
+                    let times = setTimeout(() => {
+                        statusMessage.textContent = ``
+                        clearTimeout(times)
+                    }, 5000)
+                })
+        })
+    })
 };
 
 export default sendform;
